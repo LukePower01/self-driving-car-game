6336f8f8b9cfe13b17d39334f0ab900d84a4bd56
LukePower01 <lukepower2001@icloud.com>
2024-03-04 17:23:50 +0000

    Adding comments

--- ddqn_keras.py
+++ ddqn_keras.py
@@ -6,18 +6,18 @@ import tensorflow as tf
 
 class ReplayBuffer(object):
     def __init__(self, max_size, input_shape, n_actions, discrete=False):
-        self.mem_size = max_size
+        self.mem_size = max_size # Index of last memory item
         self.mem_cntr = 0
         self.discrete = discrete
         self.state_memory = np.zeros((self.mem_size, input_shape))
-        self.new_state_memory = np.zeros((self.mem_size, input_shape))
+        self.new_state_memory = np.zeros((self.mem_size, input_shape)) 
         dtype = np.int8 if self.discrete else np.float32
-        self.action_memory = np.zeros((self.mem_size, n_actions), dtype=dtype)
+        self.action_memory = np.zeros((self.mem_size, n_actions), dtype=dtype) 
         self.reward_memory = np.zeros(self.mem_size)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.float32) # expected feature award for terminal stateis 0
 
-    def store_transition(self, state, action, reward, state_, done):
-        index = self.mem_cntr % self.mem_size
+    def store_transition(self, state, action, reward, state_, done): # state_ is the new state
+        index = self.mem_cntr % self.mem_size # Keeps memory finite
         self.state_memory[index] = state
         self.new_state_memory[index] = state_
         # store one hot encoding of actions, if appropriate
@@ -31,7 +31,7 @@ class ReplayBuffer(object):
         self.terminal_memory[index] = 1 - done
         self.mem_cntr += 1
 
-    def sample_buffer(self, batch_size):
+    def sample_buffer(self, batch_size): # sample some subset of the NN
         max_mem = min(self.mem_cntr, self.mem_size)
         batch = np.random.choice(max_mem, batch_size)
 
